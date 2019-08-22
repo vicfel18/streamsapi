4ae418b4dbc60cefd1d1f25ebe9f4933cbda5c6e
Spyro1001 <raymondhedgecock@gmail.com>
2019-08-21 20:04:44 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Java Streams API Tutorial Starter Code
+
+Use this template program to run the tutorial at
+
+`https://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/`
+
+Note:  The Person class has already been added that is used in the Advanced examples.
---
+++ src/main/java/com/example/StreamsDriver.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamsDriver {
+
+    public static void main(String[] args) {
+
+        List<String> myList =
+                Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+        myList
+                .stream()
+                .filter(s -> s.startsWith("c"))
+                .map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
+
+        // C1
+        // C2
+
+        // TODO add more tutorial code here
+
+    }
+}
